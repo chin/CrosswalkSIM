3835f2a63277f74ba3f2fcfff1092ecaab02de58
Chinyere Isaac-Heslop <cisaache@bb136-11.mines.edu>
2017-11-25 19:56:01 -0700

    fixed self on safetySignals

--- classes/safety_signals.py
+++ classes/safety_signals.py
@@ -1,6 +1,3 @@
-#import sys
-#sys.path.append('crosswalk')
-#from SIM import getNext_ButtonTraceFile_UniformRand
 from classes import event as e
 from classes import ped as p
 from enum import Enum
@@ -40,11 +37,11 @@ class safety_signals:
     def button_press(self, request_pushed):
         
         if self.safetySignal is crosswalksignal.GREEN_GO_YELLOW_ON_PRESS:
-            if request_pushed:
-                yellow_begins(self)
+            if self.request_pushed:
+                self.yellow_begins(self)
             
         elif self.safetySignal is crosswalksignal.GREEN_GO_YELLOW_ON_TIMER:
-            yellow_begins(self)
+            self.yellow_begins(self)
         
         elif self.safetySignal is crosswalksignal.YELLOW_NO_WALK:
             pass
@@ -61,18 +58,18 @@ class safety_signals:
                 if p.ped.can_cross( peds ):
                     event_list.put( e.event(t + p.ped.exit_time( peds ), e.event.event_type.PED_EXIT, peds.id ) )
         else:
-            wrp = walk_request_pushed( pedNum ) #signal in no_walk state
-            button_press(self, wrp)
+            wrp = self.walk_request_pushed( pedNum ) #signal in no_walk state
+            self.button_press(self, wrp)
         #return self
     
     def is_impatient(): #ped is self here
         for peds in ped_list:
-            if (t-peds.arrivalTime) >= 60:
+            if (t - peds.arrivalTime) >= 60:
                 event_list.put( e.event( t + 60, e.event.event_type.PED_IMPATIENT, peds.id ) )
 
     def ped_impatient(self):
-        wrp = walk_request_pushed( pedNum )
-        button_press(self, wrp)
+        wrp = self.walk_request_pushed( pedNum )
+        self.button_press(self, wrp)
         #return self
 
     def yellow_begins(self):
@@ -81,11 +78,11 @@ class safety_signals:
         #return self
 
     def yellow_expires(self):
-        red_begins(self)
+        self.red_begins(self)
         #return self
 
     def red_expires(self):
-        green_begins(self)
+        self.green_begins(self)
         #return self
 
     def red_begins(self):
@@ -105,15 +102,15 @@ class safety_signals:
             pass
         #return self
 
-    def button_prob(n):
+    def button_prob(self, n):
         if n is 0:
             return (15/16)
         else: # n > 0
             return (1/(n+1))
 
-    def walk_request_pushed(n):
+    def walk_request_pushed(self, n):
         u = i.input.getNext_ButtonTracefile_UniformRand(i) #def from SIM file
-        if u < button_prob(n):
+        if u < self.button_prob(n):
             return True
         else:
             return False
